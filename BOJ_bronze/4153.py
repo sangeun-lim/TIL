7b760f78204e662ecc6468c7a0d1d00efd24e3af
sangeun <lse2625@gmail.com>
2022-02-08 22:47:45 +0900

    0208

---
+++ BOJ_bronze/4153.py
@@ -0,0 +1,10 @@
+while True:
+    tri = list(map(int,input().split()))
+    m = max(tri)
+    if sum(tri) == 0 :
+        break
+    tri.remove(m)
+    if tri[0]**2 + tri[1]**2 == m**2 :
+        print('right')
+    else:
+        print('wrong')
