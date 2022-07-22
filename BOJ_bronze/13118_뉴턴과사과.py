dd45622d177d26cc7f87d07dc039fae7b6fec051
sangeun <lse2625@gmail.com>
2022-07-22 20:40:35 +0900

    0722

---
+++ BOJ_bronze/13118_뉴턴과사과.py
@@ -0,0 +1,12 @@
+p1,p2,p3,p4 = map(int,input().split())
+x,y,r = map(int,input().split())
+if x == p1 :
+    print(1)
+elif x == p2:
+    print(2)
+elif x == p3:
+    print(3)
+elif x == p4:
+    print(4)
+else:
+    print(0)
