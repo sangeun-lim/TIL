9400d605ff56bd6752205fd456760baeb35a9ead
sangeun <lse2625@gmail.com>
2022-09-16 18:39:54 +0900

    0916

---
+++ BOJ_bronze/4504_배수찾기.py
@@ -0,0 +1,9 @@
+n = int(input())
+while True:
+    x = int(input())
+    if x == 0 :
+        break
+    if x % n == 0 :
+        print(f'{x} is a multiple of {n}.')
+    else:
+        print(f'{x} is NOT a multiple of {n}.')
