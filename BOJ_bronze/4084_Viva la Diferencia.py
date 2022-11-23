06e70f67f4c400191d8041db5a16f19024e02bcb
임상은 <63418624+sangeun-lim@users.noreply.github.com>
2022-11-23 14:54:42 +0900

    1123

---
+++ BOJ_bronze/4084_Viva la Diferencia.py
@@ -0,0 +1,10 @@
+while True:
+    a,b,c,d = map(int,input().split())
+    if a == b == c == d == 0 :
+        break
+    cnt = 0
+
+    while not (a == b == c == d):
+            cnt += 1
+            a,b,c,d = abs(a-b), abs(b-c), abs(c-d), abs(d-a)
+    print(cnt)
