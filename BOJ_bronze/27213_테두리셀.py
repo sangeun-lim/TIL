a632e0ddf1dca7c06c5bc7439d463e0fdbaf57b8
sangeun <lse2625@gmail.com>
2023-01-28 20:04:16 +0900

    0128

---
+++ BOJ_bronze/27213_테두리셀.py
@@ -0,0 +1,9 @@
+m = int(input())
+n = int(input())
+if m == 1 or n == 1:
+    print(m*n)
+# elif (m == 2 and n <=3) or (n==2 and m <= 3):
+#     print(m*n)
+else:
+    result = (m+n)*2 - 4
+    print(result)
