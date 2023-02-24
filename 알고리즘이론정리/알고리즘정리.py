375deb09a1cbbeea06ef8082553a1850d9f484a4
sangeun <lse2625@gmail.com>
2023-02-25 02:26:56 +0900

    0225 알고리즘 정리

--- 알고리즘이론정리/알고리즘정리.py
+++ 알고리즘이론정리/알고리즘정리.py
@@ -777,24 +777,155 @@ topology_sort()
 
 #--------------------------------------------------------------------#
 
+# 소수 판별 알고리즘 : 기본
+def is_prime_number(x):
+    # 2부터 (x-1)까지의 모든 수를 확인하며
+    for i in range(2,x):
+        # x 가 해당 수로 나누어떨어진다면
+        if x % i == 0:
+            return False # 소수가 아님
+    return True # 소수임
 
+print(is_prime_number(4))
+print(is_prime_number(7))
+
+#--------------------------------------------------------------------#
+
+# 소수 판별 알고리즘 : 개선
+import math
+
+def is_prime_number(x):
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0 :
+            return False
+    return True
+
+print(is_prime_number(4))
+print(is_prime_number(7))
+
+#--------------------------------------------------------------------#
+
+# 다수의 소수 판별
+# 에라토스테네스의 체
+# 1. 2부터 N까지 모든 자연수 나열
+# 2. 남은 수 중 아직 처리하지 않은 가장 작은 수 i를 찾음
+# 3. 남은 수 중에서 i의 배수를 모두 제거 (i는 제거하지 않음)
+# 4. 2번과 3번 반복
+
+import math
+
+n = 1000 # 2부터 1000까지의 모든 수에 대하여 소수 판별
+# 처음엔 모든 수가 소수(True)인것으로 초기화(0과 1은 제외)
+array = [True for i in range(n+1)]
+
+# 에라토스테네스의 체 알고리즘 수행
+for i in range(2, int(math.sqrt(n)) + 1):
+    if array[i] == True: # i가 소수인 경우 (남은 수인 경우)
+        # i를 제외한 i의 모든 배수를 지우기
+        j = 2
+        while i * j <= n:
+            array[i*j] = False
+            j += 1
+# 모든 소수 출력
+for i in range(2, n+1):
+    if array[i]:
+        print(i, end=' ')
+
+#--------------------------------------------------------------------#
+
+# 투 포인터 (ex문제: 특정한 합을 가지는 부분 연속 수열 찾기)
+n = 5 # 데이터의 개수
+m = 5 # 찾고자 하는 부분합
+data = [1,2,3,2,5] # 전체수열
+
+cnt = 0
+interval_sum = 0
+end = 0
+
+for start in range(n):
+    # end를 가능한 만큼 이동시키기
+    while interval_sum < m and end < n:
+        interval_sum += data[end]
+        end += 1
+    # 부분합이 m일 때 카운트 증가
+    if interval_sum == m:
+        cnt += 1
+    interval_sum -= data[start]
+
+print(cnt)
+
+#--------------------------------------------------------------------#
+
+# 구간 합(접두사 합(prefix sum) 이용하기)
+n = 5
+data = [10,20,30,40,50]
+
+# 접두사 합(prefix sum) 배열 계산
+sum_value = 0
+prefix_sum = [0]
+for i in data:
+    sum_value += i
+    prefix_sum.append(sum_value)
+
+# 구간합 계산
+left = 3
+right = 4
+print(prefix_sum[right] - prefix_sum[left-1])
+
+#--------------------------------------------------------------------#
+
+# 트리의 순회
+
+class Node:
+    def __init__(self, data, left_node, right_node):
+        self.data = data
+        self.left_node = left_node
+        self.right_node = right_node
+
+# 전위 순회(Preorder Traversal)
+def pre_order(node):
+    print(node.data, end=' ')
+    if node.left_node != None:
+        pre_order(tree[node.left_node])
+    if node.right_node != None:
+        pre_order(tree[node.right_node])
+
+# 중위 순회(Inorder Traversal)
+def in_order(node):
+    if node.left_node != None:
+        pre_order(tree[node.left_node])
+    print(node.data, end=' ')
+    if node.right_node != None:
+        pre_order(tree[node.right_node])
+
+# 후위 순회(Postorder Traversal)
+def post_order(node):
+    if node.left_node != None:
+        pre_order(tree[node.left_node])
+    if node.right_node != None:
+        pre_order(tree[node.right_node])
+    print(node.data, end=' ')
+
+n = int(input())
+tree = {}
+
+for i in range(n):
+    data, left_node, right_node = input().split()
+    if left_node == 'None':
+        left_node = None
+    if right_node == 'None':
+        right_node = None
+    tree[data] = Node(data, left_node, right_node)
+
+pre_order(tree['A'])
+print()
+in_order(tree['A'])
+print()
+post_order(tree['A'])
 
 #--------------------------------------------------------------------#
 
 
 
 #--------------------------------------------------------------------#
-
-
-
-#--------------------------------------------------------------------#
-
-
-
-#--------------------------------------------------------------------#
-
-
-
-
-
 
